01f92ce9fbe51f1b7cf042544aa9f8cf7d7ed0f6
Jayanta Mahanty <237518800+mahantyjayanta340-collab@users.noreply.github.com>
2026-09-06 22:11:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'classradar-v4-clean';
+const CACHE_NAME = 'classradar-v5-clean';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
